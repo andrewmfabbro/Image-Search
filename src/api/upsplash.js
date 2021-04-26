3a9a6291775b6f0c8bb451723d6295ab5c49e7ad
Andrew <andrewmfabbro@gmail.com>
2021-04-26 17:03:04 -0400

    Update upsplash.js
    added comment for api auth spot

--- src/api/upsplash.js
+++ src/api/upsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      "Client-ID b7888d6147084f411e61f5966a098723bc6777e4ff0b3694780a40b4cbbf05a4"
+      //add the client auth here
   }
 });
